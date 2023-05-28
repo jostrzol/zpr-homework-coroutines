8ff55b4824051c094995c0ccc3802fe7c7276d95
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2023-05-28 15:46:55 +0200

    przykład 3

--- coroutines.cpp
+++ coroutines.cpp
@@ -1,8 +1,10 @@
 // autor:       Jakub Ostrzołek
 // opis:        zadanie dodatkowe ZPR - C++20 coroutines
-// kompilacja:  g++ -fcoroutines coroutines.cpp
+// kompilacja:  g++ -fcoroutines -std=c++20 coroutines.cpp
 
 #include <coroutine>
+#include <cstdint>
+#include <exception>
 #include <iostream>
 
 // Coroutine to koncepcja znana z innych języków, takich jak JavaScript, Kotlin,
@@ -11,14 +13,17 @@
 // przez kod wywołujący taką funkcję.
 //
 // Implementacje w innych językach są zazwyczaj łatwiejsze w użyciu ze względu
-// na obecność garbage collectora. W C++ nie ma takiego mechanizmu i trzeba
-// zadbać o poprawne czyszczenie wcześniej wspomnianego obiektu na stercie.
+// na obecność garbage collectora oraz prostsze paradygmaty języków. W C++ nie
+// ma takiego mechanizmu i trzeba zadbać o poprawne czyszczenie wcześniej
+// wspomnianego obiektu na stercie (co jednak może zostać zautomatyzowane za
+// pomocą wzorca RAII).
 //
 // Coroutines C++20 nie są trywialne w użyciu. Aby zdefiniować coroutine należy
 // zdefiniować funkcję spełniającą 2 nadrzędne warunki:
-//  * użyć jednego z nowo wprowadzonych operatorów: co_await, co_yield lub
-//  co_return,
-//  * posiadać typ zwracany zgodny z pewnymi warunkami
+//  * używa przynajmniej jednego z nowo wprowadzonych operatorów: co_await,
+//    co_yield lub co_return,
+//  * posiada typ zwracany zgodny z pewnymi warunkami - będą one omówione na
+//    poniższych przykładach.
 
 // 1. Prosty przykład - licznik wypisujący na ekran kolejne liczby.
 namespace p1 {
@@ -146,11 +151,11 @@ struct coroutine : std::coroutine_handle<promise> {
 struct promise {
   // Dodajemy wartość do klasy promise, którą coroutine może zapisywać przy
   // wywołaniu co_yield `expr`.
-  std::size_t value;
+  std::size_t value_;
 
   // Dodajemy wartość do klasy promise, którą coroutine może zapisywać przy
   // wywołaniu co_return `expr`.
-  std::string ret;
+  std::string ret_;
 
   coroutine get_return_object() { return {coroutine::from_promise(*this)}; }
 
@@ -161,18 +166,18 @@ struct promise {
 
   // Zamieniamy return_void() na return_value(), aby móc zapisać wartość do pola
   // ret przy zakończeniu coroutine.
-  void return_value(std::string &&ret_value) { ret = ret_value; }
+  void return_value(std::string &&value) { ret_ = value; }
 
   // Ta funkcja będzie wołana przy wywołaniu operatora co_yield `expr`.
   //
-  // Tutaj zapisujemy przekazaną wartość w polu value, a następnie zwracamy
+  // Tutaj zapisujemy przekazaną wartość w polu value_, a następnie zwracamy
   // obiekt std::suspend_always, dzięki czemu coroutine się zawiesi każdorazaowo
   // po wywpołaniu co_yield `expr`. Oczywiście możemy zwracać własny 'awaiter' w
   // celu optymalizacji (być może coroutine wcale nie musi się od razu zawiesić,
   // a policzyć przyszłe wartości, co oszczędziłoby każdorazowego, kosztownego
   // zapisywania stanu funkcji do sterty).
   std::suspend_always yield_value(std::size_t i) {
-    value = i;
+    value_ = i;
     return {};
   }
   void unhandled_exception() {}
@@ -194,27 +199,139 @@ coroutine counter(std::size_t max) {
 auto main() -> void {
   auto h = counter(3);
   auto &p = h.promise();
+
+  // W warunku pętli sprawdzamy czy coroutine się zakończyła
+  //
+  // UWAGA: niepoprawne byłoby użycie domyślnej konwersji obiekty
+  // std::coroutine_handle<...> na bool (operator bool) - sprawdza on jednie czy
+  // coroutine_handle jest zainicjowany a nie czy coroutine dalej żyje.
   while (!h.done()) {
     // Zamieniliśmy kolejność, najpierw wypisujemy, potem wznawiamy coroutine.
     // Możemy tak zrobić, ponieważ teraz promise::initial_suspend zwraca
     // std::suspend_never i wartość jest wyliczona przy tworzeniu coroutine.
-    std::cout << "main: got from coroutine: " << p.value << std::endl;
+    std::cout << "main: got from coroutine: " << p.value_ << std::endl;
     h();
   }
+
   // Tutaj coroutine musiała zakończyć działanie, bo h.done() == true. Zatem
   // możemy przeczytać wartość zwróconą.
-  std::cout << "main: coroutine endend: " << p.ret << std::endl;
+  std::cout << "main: coroutine endend: " << p.ret_ << std::endl;
 
   h.destroy();
   return;
 }
 } // namespace p2
 
+// 3. Przykład - generyczny szablon coroutine.
+namespace p3 {
+
+// Aby ułatwić sobie życie możemy stworzyć generyczny szablon wyżej omawianej
+// klasy coroutine jej wewnętrznej klasy promise_type. Szablon będzie dbał
+// również o zwolnienie pamięci w destruktorze.
+template <typename T> struct Generator {
+  struct promise_type;
+  using handle_type = std::coroutine_handle<promise_type>;
+
+  struct promise_type {
+    T value_;
+    std::exception_ptr exception_;
+
+    Generator get_return_object() { return {handle_type::from_promise(*this)}; }
+    std::suspend_always initial_suspend() { return {}; }
+    std::suspend_always final_suspend() noexcept { return {}; }
+
+    // Zapamiętujemy wyjątek, aby rzucić go później w funkcji
+    // Generator<...>::fill(). W ten sposób wyjątek przepropaguje do kodu
+    // wołającego coroutine.
+    void unhandled_exception() { exception_ = std::current_exception(); }
+
+    // Korzystamy z konceptów c++20, aby można było wywołać co_yield z
+    // jakąkolwiek wartością konwertowalną do oczekiwanej wartości.
+    template <std::convertible_to<T> From>
+    std::suspend_always yield_value(From &&from) {
+      value_ = std::forward<From>(from);
+      return {};
+    }
+    void return_void() {}
+  };
+
+  // Tym razem zamiast dziedziczyć po std::coroutine_handle<...> agregujemy go.
+  handle_type h_;
+
+  Generator(handle_type h) : h_(h) {}
+  // Zwalniamy pamięć zaalokowaną na stercie
+  ~Generator() { h_.destroy(); }
+
+  // Tutaj operator bool sprawdza czy coroutine zakończyła działanie.
+  //
+  // Aby to sprawdzić musimy najpierw wznowić coroutine (za pomocą
+  // Generator<...>::fill()). Jeżeli coroutine wywołała co_yield, to wynik
+  // zostanie zapamiętany w promise_type::value_, a h_.done() == false. W
+  // przeciwnym wypadku pole promise_type::value_ pozostanie niezmienione, a
+  // h_.done() == true.
+  explicit operator bool() {
+    fill();
+    return !h_.done();
+  }
+
+  // operator() przenosi ostatnio zapisaną wartość w promise_type::value_.
+  // Jeżeli wartość była 'przeterminowana' (full_ == false), to również
+  // wznawiamy coroutine i pobieramy nową wartość.
+  T operator()() {
+    fill();
+    full_ = false;
+    return std::move(h_.promise().value_);
+  }
+
+private:
+  // Aby zabezpieczyć się przed przypadkowym nadpisaniem promise_type::value_ za
+  // pomocą podwójnego wywołania Generator<...>::operator bool, zapamiętujemy
+  // stan - czy obecnie przetrzymywana wartość w promise_type::value_ jest
+  // prawidłowa i można ją wyciągnąć za pomocą Generator<...>::operator(), czy
+  // powinniśmy wczytać nową wartość. Logikę tę realizuje funkcja
+  // Generator<...>::fill().
+  bool full_ = false;
+
+  void fill() {
+    if (!full_) {
+      // Wznawiamy coroutine.
+      h_();
+
+      // Propagujemy wyjątek do kodu wywołującego coroutine, jeżeli wystąpił.
+      if (h_.promise().exception_)
+        std::rethrow_exception(h_.promise().exception_);
+
+      full_ = true;
+    }
+  }
+};
+
+Generator<size_t> counter(std::size_t max) {
+  for (std::size_t i = 0; i < max; i++) {
+    std::cout << "coroutine: generated: " << i << std::endl;
+    co_yield i;
+  }
+}
+
+auto main() -> void {
+  auto gen = counter(3);
+  while (gen) {
+    std::cout << "main: got from coroutine: " << gen() << std::endl;
+  }
+
+  // Nie musimy niszczyć obiektu stanu coroutine, bo zostanie on zniszczony
+  // razem z generatorem.
+  return;
+}
+} // namespace p3
+
 auto main() -> int {
   std::cout << "<--- p1 --->" << std::endl;
   p1::main();
   std::cout << "<--- p2 --->" << std::endl;
   p2::main();
+  std::cout << "<--- p3 --->" << std::endl;
+  p3::main();
   return 0;
 }
 
